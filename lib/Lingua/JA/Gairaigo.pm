8b09a8857186e7d16e7fa27d4cb3b0a2b2194491
Ben Bullock <benkasminbullock@gmail.com>
2013-03-23 17:13:35 +0900

    Initial commit of Lingua-JA-Gairaigo

---
+++ lib/Lingua/JA/Gairaigo.pm
@@ -0,0 +1,27 @@
+=encoding UTF-8
+
+=head1 NAME
+
+Lingua::JA::Gairaigo - abstract here.
+
+=head1 SYNOPSIS
+
+    use Lingua::JA::Gairaigo;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
+package Lingua::JA::Gairaigo;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+use Carp;
+our $VERSION = 0.01;
+1;
---
+++ t/Lingua-JA-Gairaigo.t
@@ -0,0 +1,10 @@
+# This is a test for module Lingua::JA::Gairaigo.
+
+use warnings;
+use strict;
+use Test::More;
+use Lingua::JA::Gairaigo;
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
